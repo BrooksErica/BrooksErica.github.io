8f800a89b98d0a4c70f53856b734a4b33687f73e
BrooksErica <132095882+BrooksErica@users.noreply.github.com>
2025-02-07 16:22:42 +0000

    Deploying to gh-pages from @ BrooksErica/BrooksErica.github.io@31f0a5acaff002be0631b09f0605c94e8a1e6704 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,6 +445,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-case-study-1",
+          title: 'case-study 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Case-Study-1/";
             },},{
         id: 'social-email',
         title: 'email',
